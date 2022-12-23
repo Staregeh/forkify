05cc3de22d98b1187d86e92cd8fcfcf6febdaad7
staregeh <kushkaoleg@gmail.com>
2022-12-23 14:57:26 +0200

    initial

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '53d71856-198e-4431-9d69-bbd38ca9e53e';
+export const MODAL_CLOSE_SEC = 2.5;
